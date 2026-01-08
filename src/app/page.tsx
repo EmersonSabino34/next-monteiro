f0f88b346fca80e8721e0adfb3bec442938446b0
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-08 19:49:58 +0000

    Fix formatting in menuItems array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ReservationForm from '@/components/ReservationForm/ReservationForm';
 export default function Home() {
   const menuItems = [
 
-    
+
     {
       image: '/img/picanha.jpg',
       title: 'Picanha Premium',
