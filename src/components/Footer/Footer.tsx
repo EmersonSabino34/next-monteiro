061bfa5bdf1811411b26a659f0ad5d52ae006a88
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-02-03 20:20:16 +0000

    Add footer element to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,6 +2,7 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
+    
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div>
